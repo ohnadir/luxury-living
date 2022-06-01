a59d2ba448aa30ac0efba1f4cb6c6955f584ee93
ohnadir <qx.nadir@gmail.com>
2022-06-01 22:48:57 +0600

    add navbar and all page to the route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Page/Shared/Navbar';
+import Home from './Page/Home/Home';
+import About from './Page/About';
+import Project from './Page/Projects';
+import Contact from './Page/Contact';
+import Admin from './Page/Admin';
+import Login from './Page/Login/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is Luxury </h1>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/projects' element={<Project></Project>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/admin' element={<Admin></Admin>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/Page/Shared/Navbar.js
+++ src/Page/Shared/Navbar.js
@@ -1,17 +1,43 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import CustomLink from '../Login/Login/CustomLink';
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
+    const navigate = useNavigate()
 
-    const menuList = <>
-        <Link to='/home'>Home</Link>
-        <Link to='/home'>Home</Link>
-        <Link to='/home'>Home</Link>
+    const menuLists = <>
+        <CustomLink to='/home'>Home</CustomLink>
+        <CustomLink to='/about'>About</CustomLink>
+        <CustomLink to='/projects'>Projects</CustomLink>
+        <CustomLink to='/contact'>Contact</CustomLink>
+        <CustomLink to='/admin'>Admin</CustomLink>
+        <CustomLink to='/login'>Login</CustomLink>
     </>
     return (
-        <div>
-            
+        <div className='max-w-7xl mx-auto px-4'>
+            <div className='flex items-center h-14 justify-between text-black  relative z-50'>
+                <FontAwesomeIcon
+                    icon={open ? faTimes : faBars}
+                    onClick={() => setOpen(!open)}
+                    className="text-black w-6 h-6 cursor-pointer md:hidden"/>
+                <div className=''>
+                    <span className='cursor-pointer' onClick={()=>navigate('/home')}>Luxury Living</span>
+                </div>
+                <div className='flex gap-6 items-center hidden md:flex'>
+                    {menuLists}
+                </div>
+               
+                {open && (
+                    <div className="bg-slate-600 absolute top-full left-0 flex flex-col w-full pb-8 md:hidden">
+                        <div className=" flex gap-4 flex-col items-center text-xl">
+                            {menuLists}
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
